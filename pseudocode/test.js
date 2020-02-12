802e4bfe60705f4a76bf3389767b7b74d83104a0
ArshidBaba <arshid.aslam@insisiv.com>
2020-02-12 21:31:16 +0530

    deleted some useless files

--- pseudocode/test.js
+++
@@ -1,6 +0,0 @@
-var code = document.getElementById("hello-world-code").textContent;
-var parentEl = document.body;
-var options = {
-    lineNumber: true
-};
-pseudocode.render(code, parentEl, options);
